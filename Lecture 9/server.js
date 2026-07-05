d956c3dae3b93e1f994c11ae753dd3c00c3886b2
Phantom8Assassin25 <mansabhatt62587@gmail.com>
2026-07-05 08:58:03 +0530

    Update server.js

--- Lecture 9/server.js
+++ Lecture 9/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
-mongoose.connect("mongodb+srv://mansabhatt62587_db_user:Paasword:cluster7.klfzxyk.mongodb.net/")
+mongoose.connect("mongodb+srv://accountname_db_user:Password:cluster0.klfzxyk.mongodb.net/")
 .then(() =>console.log("DB Connected"))
 .catch(err =>console.log(err));
 
